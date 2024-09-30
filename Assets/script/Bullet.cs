b4c9bef6434e0cf00c052e3fa7d789ae08935d8b
TaguchiRei <ren.len2426@gmail.com>
2024-09-30 19:30:23 +0900

    postprocess追加 多少ファイル変更もあったので全体をコミット

---
+++ Assets/script/Bullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] Rigidbody _rb;
+    GameObject _muzzle;
+    public float bulletSpeed = 1;
+    float timer = 5;
+    private void Start()
+    {
+        _muzzle = GameObject.Find("muzzle");
+        transform.SetPositionAndRotation(_muzzle.transform.position, _muzzle.transform.rotation);
+        _rb.velocity = transform.forward * bulletSpeed;
+        timer = 5;
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/script/PlayerMove.cs
+++ Assets/script/PlayerMove.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
@@ -5,11 +6,18 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] float _moveSpeed = 1f;
     [SerializeField] float _jumpPower = 1f;
     [SerializeField] float _hookEjectionForce = 1f;
+    [SerializeField] float shotInterval = 0.1f;
+    float shotIntervalTime = 0;
 
+    [SerializeField] GameObject _bullet;
+
+    [SerializeField] GameObject _fpsHand;
+    [SerializeField] MakePendulum _makePendulum;
     [SerializeField] Rigidbody _rig;
 
     Vector3 _movePower = Vector3.zero;
     bool _onGround = true;
+    bool _gunMode = true;
 
     private void Start()
     {
@@ -19,18 +27,33 @@ public class PlayerMove : MonoBehaviour
     private void Update()
     {
         _movePower = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        //逆向きに力を加えて動きを止める
-        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
-        {
-            if (_onGround)
-            {
-                _rig.velocity = Vector3.zero;
-            }
-        }
+        
         if (_onGround && Input.GetButtonDown("Jump"))
         {
             _movePower += new Vector3(0, _jumpPower, 0);
             _onGround = false;
+        }
+        //射撃
+        if (Input.GetButton("Fire1"))
+        {
+            if(_gunMode && shotIntervalTime <= 0)
+            {
+                Instantiate(_bullet);
+                shotIntervalTime = shotInterval;
+            }
+            else
+            {
+
+            }
+        }
+        if(shotIntervalTime > 0)
+        {
+            shotIntervalTime -= Time.deltaTime;
+        }
+        //フックショット射撃
+        if (Input.GetButtonDown("Fire2"))
+        {
+
         }
         //接地判定
         var line = Physics.Raycast(transform.position, Vector3.down, 1.5f);
@@ -43,6 +66,18 @@ public class PlayerMove : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _rig.AddForce(_movePower * _moveSpeed, ForceMode.Impulse);
+        if(_onGround)
+        {
+            _rig.AddForce(_movePower * _moveSpeed, ForceMode.Impulse);
+            _rig.velocity = (_moveSpeed / Mathf.Abs(_rig.velocity.x) + Mathf.Abs(_rig.velocity.y) + Mathf.Abs(_rig.velocity.z)) * _rig.velocity;
+            if (Mathf.Abs(_rig.velocity.x) + Mathf.Abs(_rig.velocity.y) + Mathf.Abs(_rig.velocity.z) < 10)
+            {
+                _rig.velocity = new Vector3();
+            }
+        }
+        else
+        {
+            _rig.AddForce(_movePower * _moveSpeed / 2, ForceMode.Acceleration);
+        }
     }
 }
