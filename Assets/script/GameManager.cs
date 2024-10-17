dade84eb80d14570f64a0c36abb250e591ed8770
TaguchiRei <ren.len2426@gmail.com>
2024-10-17 20:32:06 +0900

    敵ターゲット機能を作成開始

--- Assets/script/GameManager.cs
+++ Assets/script/GameManager.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,8 +9,10 @@ public class GameManager : MonoBehaviour
     [HideInInspector] public static GameManager Instance = default;
     GameObject _player;
     GameObject _hpUI;
-    GameObject _EnergyUI;
+    GameObject _energyUI;
+    Camera _camera;
     [SerializeField] GameObject _enemyTarget;
+    List<GameObject> _enemys = new();
 
 
     private void Awake()
@@ -28,9 +31,23 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         _hpUI = GameObject.FindGameObjectWithTag("HP");
-        _EnergyUI = GameObject.FindGameObjectWithTag("Energy");
+        _energyUI = GameObject.FindGameObjectWithTag("Energy");
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _camera = GameObject.FindObjectOfType<Camera>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    private void Update()
+    {
+        foreach (GameObject enemy in _enemys)
+        {
+            if(enemy.GetComponent<EnemyUI>()._onTarget == false)
+            {
+                enemy.GetComponent<EnemyUI>()._onTarget = true;
+            }
+        }
     }
 
     void OnSceneLoaded(Scene s, LoadSceneMode mode)
@@ -40,10 +57,14 @@ public class GameManager : MonoBehaviour
 
     public void ShotGun(float full, float bullet)
     {
-        _EnergyUI.GetComponent<Image>().DOFillAmount((bullet / full), 0.2f);
+        _energyUI.GetComponent<Image>().DOFillAmount((bullet / full), 0.2f);
     }
     public void HpUpDown(float max, float now)
     {
         _hpUI.GetComponent<Image>().DOFillAmount(now / max, 0.2f);
+    }
+    void AddEnemy(GameObject enemy)
+    {
+        _enemys.Add(enemy);
     }
 }
